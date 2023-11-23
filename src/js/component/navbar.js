e4d7c025d62c90a004e81a75e10cf723aa275834
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-11-23 00:01:14 +0000

    Almost finished HTML and CSS

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light mb-3">
-			<Link to="/">
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button style={{marginRight:"10px"}} className="btn btn-success">Add new contact</button>
-				</Link>
-			</div>
-		</nav>
+		// <nav className="navbar navbar-light mb-3">
+		// 	<Link to="/">
+		// 	</Link>
+		// 	<div className="ml-auto">
+		// 		<Link to="/demo">
+		// 			<button style={{ marginRight: "10px" }} className="btn btn-success ">Add new contact</button>
+		// 		</Link>
+		// 	</div>
+		// </nav>
 	);
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
+
+export const Home = () => {
+	return (
+		<div>
+			<nav className="navbar navbar-light mb-3">
+				<Link to="/">
+				</Link>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button style={{ marginRight: "10px" }} className="btn btn-success ">Add new contact</button>
+					</Link>
+				</div>
+			</nav>
+			<div className="d-flex">
+				<img src="https://placehold.co/600x400" />
+				<div className="contact-info">
+					<p className="contact-name">Mike Anamendolla</p>
+					<p className="street-adress">5842 Hillcrest Red</p>
+					<p className="contact-number">(870) 288-4149</p>
+					<p className="email-direction">mike.ana@example.com</p>
+				</div>
+			</div>
+		</div>
+	)
+};
 
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-			<img src="https://placehold.co/600x400"/>
-	</div>
-);
+
