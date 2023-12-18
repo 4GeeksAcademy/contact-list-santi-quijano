727888b0ad89fd82e8e8df2f4821a257a436dc41
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-12-18 16:20:16 +0000

    Finishing Fetches

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -13,7 +13,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					initial: "white"
 				}
 			],
-			agenda: []
+			agenda: [],
+			contact: [],
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -39,13 +40,82 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//reset the global store
 				setStore({ demo: demo });
 			},
-			getAllContacts: async () => {
-				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/santiago_agenda").then(response => response.json()).then(data => {
-					setStore({ agenda: data });
-				}).catch(err => console.log(err))
+		},
+		getMyAgenda: async () => {
+			const getUrl = 'https://playground.4geeks.com/apis/fake/contact/agenda/Mermate'
+			fetch(getUrl).then(response=>response.json()).then(data=>{
+				setStore({ agenda: data});
+			})
+			.catch(error=>{
+				console.error("There was an error while fetching the agenda, try again:", error);
+			});
+		},
+		postContact: async (fullName, email, phone, address) => {
+			try {
+				const store = getStore();
+				const addContact = {
+					fullName: fullName,
+					email: email,
+					agenda_slug: 'Mermate',
+					phone: phone,
+					adress: address
+				};
+				const response = await fetch('https://playground.4geeks.com/apis/fake/contact/', {
+					method: "POST",
+					body: JSON.stringify(addContact),
+					headers: {"Content-Type": "application/json"}
+				});
+				if (response.ok) {
+					console.log("Contact successfully created!");
+				} else {
+					console.error("There was an error while creating your contact, try again:", response.status);
+				}
+			} catch (error) {
+				console.error("There was an error in your solicitude, try again:", error)
 			}
+		},
+		putContact: (fullName, email, phone, address, id) => {
+			const updateContact = {
+				fullName: fullName,
+				email: email,
+				agenda_slug: 'Mermate',
+				phone: phone,
+				address: address
+			}
+			try {
+				const response = fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`,{
+					method: "PUT",
+					headers: {"Content-Type": "application/json"},
+					body: JSON.stringify(updateContact)
+				});
+				if(response.ok) {
+					console.error("Contact successfully updated!");
+				} else {
+					console.error("There was an error while updating your contact, try again:", response.status);
+				}
+			} catch (error) {
+			console.error("There was an error in your solicitude:", error);
 		}
+		actions.getMyAgenda() 
+	 	},
+		getContact: (id) => {
+			const contactUrl = `https://playground.4geeks.com/apis/fake/contact/${id}`
+			fetch(contactUrl).then(response=> response.json()).then(data=>{
+				setStore({ contact: data});
+				console.log(data)
+			})
+			.catch(error=>{
+				console.error("Error fetching contact:", error);
+			});
+			getContact: (index) => {
+				const store = getStore();
+				setStore({ contact: store.agenda[index]})
+				console.log(store.contact)
+			}
+		},
+			
 	};
 };
 
 export default getState;
+
